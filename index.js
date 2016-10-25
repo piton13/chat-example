58a5be1a9d33a3cbebc8cfa7cbf07e655cfa63ad
Igor Mykhaylyuk <igor_mykhaylyuk@epam.com>
2016-10-25 11:43:02 +0300

    my first init commit

--- index.js
+++ index.js
@@ -2,16 +2,23 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
-  });
+io.on('connection', function(socket) {
+    // io.emit('some event', { for: 'everyone' });
+    // socket.emit('my other event', { my: 'data' });
+    // socket.emit('news', { hello: 'world' });
+
+    // socket.on('chat message', function(msg) {
+    //     socket.broadcast.emit(msg);
+    // });
+    socket.on('chat message', function(msg) {
+        io.emit('chat message', msg);
+    });
 });
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+http.listen(3000, function() {
+    console.log('listening on *:3000');
 });
